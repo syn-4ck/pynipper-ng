e80f7899acf7e9364a4cd937717f12c5e3a5b87e
Julian (syn-4ck) <repojfm@protonmail.com>
2022-07-24 21:47:59 +0200

    QA Flake 8 fix

--- src/analyze/cisco/ios/plugins/dns_plugin.py
+++ src/analyze/cisco/ios/plugins/dns_plugin.py
@@ -31,10 +31,10 @@ class PluginDNS(GenericPlugin):
         if (not self._has_dns_enabled(filename) and not self._has_no_domain_lookup(filename)):
             return CiscoIOSIssue(
                 "Domain Lookups",
-                "Cisco IOS-based devices support name lookups using the DNS. However, if a DNS server has not been configured, then the DNS request is broadcast.It is determined that name lookups had not been disabled and no DNS servers had been configured.",
-                "An attacker who was able to capture network traffic could monitor DNS queries from the device. Furthermore, Cisco devices can connect to Telnet servers by supplying only the hostname or IP address of the server. A mistyped Cisco command could be interpreted as an attempt to connect to a Telnet server and broadcast on the network.",
-                "It would be trivial for an attacker to capture network traffic broadcast from a device. Furthermore, network traffic capture tools are widely available on the Internet.",
-                "It is recommends that domain lookups be disabled. Domain lookups can be disabled with the following command: no ip domain-lookup. If domain lookups are required, It is recommends that DNS be configured. DNS can be configured with the following command: ip name-server <IP address>."
+                "Cisco IOS-based devices support name lookups using the DNS. However, if a DNS server has not been configured, then the DNS request is broadcast.It is determined that name lookups had not been disabled and no DNS servers had been configured.",  # noqa: E501
+                "An attacker who was able to capture network traffic could monitor DNS queries from the device. Furthermore, Cisco devices can connect to Telnet servers by supplying only the hostname or IP address of the server. A mistyped Cisco command could be interpreted as an attempt to connect to a Telnet server and broadcast on the network.",  # noqa: E501
+                "It would be trivial for an attacker to capture network traffic broadcast from a device. Furthermore, network traffic capture tools are widely available on the Internet.",  # noqa: E501
+                "It is recommends that domain lookups be disabled. Domain lookups can be disabled with the following command: no ip domain-lookup. If domain lookups are required, It is recommends that DNS be configured. DNS can be configured with the following command: ip name-server <IP address>."  # noqa: E501
             )
 
     def analyze(self, config_file) -> None:
